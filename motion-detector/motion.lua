6778fbbb4e781a792e3e06e1b8790365cbe65787
Eric Turcotte <eturcotte@climate.com>
2015-09-04 20:18:48 -0500

    Using a table to try to fix durations that are sometimes negative Sending duration-since-start-s parameter Cleaned up logging

--- motion-detector/motion.lua
+++ motion-detector/motion.lua
@@ -1,15 +1,19 @@
 local inpin = 6
 gpio.mode(inpin, gpio.INT)
 
-start = tmr.time()
-lastEnd = 0
-
 local PORT = 8080
 local URI = "/iot/v1/rosie/motion"
 local device_id = node.chipid()
 
-function build_request(d)
-    local body = '{"duration-s": ' .. d .. ', "device-id": "' .. device_id .. '"}'
+function debug(message)
+    print("[DEBUG] " .. message)
+end
+
+function build_request(d, start)
+    local duration_since_start = tmr.time() - start
+    debug("duration since start " .. duration_since_start)
+    local body = '{"duration-s": ' .. d .. ', "duration-since-start-s": ' .. duration_since_start ..
+            ', "device-id": "' .. device_id .. '"}'
 
     local request = "POST " .. URI .. " HTTP/1.1\r\n" ..
             "Host: " .. HOST .. "\r\n" ..
@@ -19,74 +23,72 @@ function build_request(d)
             "Content-Length: " .. string.len(body) .. "\r\n\r\n" ..
             body .. "\r\n"
 
-    print(request)
+    debug(request)
 
     return request
 end
 
-function on_receive(sck, payload)
-    print("Response " .. payload)
+function on_receive(_, payload)
+    debug("Response " .. payload)
 end
 
-function on_reconnection(sck)
-    print("Reconnection")
-end
-
-function on_disconnection(sck)
-    print("Disconnection")
-end
-
-function on_sent(sck)
-    print("Sent")
-end
-
-function send_duration(d)
-    print("duration " .. d)
+function send_duration(duration, start)
+    debug("duration " .. duration .. " start " .. start)
 
     if wifi.sta.status() < 5 then
-        print("wifi not connected " .. wifi.sta.status())
-        tmr.alarm(0, 5000, 0, function () send_duration(d) end)
-        print("alarm set")
+        debug("wifi not connected " .. wifi.sta.status())
+        tmr.alarm(0, 5000, 0, function() send_duration(duration, start) end)
+        debug("alarm set")
     else
-        conn = net.createConnection(net.TCP, 0)
+        local conn = net.createConnection(net.TCP, 0)
 
         conn:on("connection", function(conn)
-            local request = build_request(d)
+            local request = build_request(duration, start)
 
-            print("sending")
+            debug("sending")
             conn:send(request)
-            print("sent")
+            debug("sent")
         end)
         conn:on("receive", on_receive)
-        conn:on("reconnection", on_reconnection)
-        conn:on("disconnection", on_disconnection)
-        conn:on("sent", on_sent)
 
-        print("connecting to http://" .. HOST .. ":" .. PORT .. URI)
+        debug("connecting to http://" .. HOST .. ":" .. PORT .. URI)
         conn:connect(PORT, HOST)
-        print("connected")
+        debug("connected")
     end
 end
 
 function motion_start()
-    start = tmr.time()
-    print("Motion detected!")
-    print("[DEBUG] start: " .. start .. ", last: " .. lastEnd ..
-            ", break: " .. start - lastEnd)
-    gpio.trig(inpin, "down", motion_stop)
+    if stopwatch.started then
+        debug("stopwatch already started")
+    else
+        stopwatch.started = true
+        stopwatch.start = tmr.time()
+        debug("Motion detected!")
+        debug("start: " .. stopwatch.start)
+        gpio.trig(inpin, "down", motion_stop)
+    end
 end
 
 function motion_stop()
-    lastEnd = tmr.time()
-    local d = lastEnd - start
-    print("motion ended " .. lastEnd .. " after " .. d .. " seconds.")
-    print("[DEBUG] start: " .. start .. ", duration: " .. d)
-    print("after delay")
-    print("sending duration")
-    send_duration(d)
-    print("duration sent")
-    gpio.trig(inpin, "up", motion_start)
-    print("resetting pin")
+    if not stopwatch.started then
+        debug("stopwatch already stopped")
+    else
+        local last_end = tmr.time()
+        local last_start = stopwatch.start
+        local duration = last_end - last_start
+        debug("motion started " .. last_start .. " ended " .. last_end .. " after " .. duration .. " seconds.")
+        debug("after delay")
+        debug("sending duration")
+        send_duration(duration, last_start)
+        debug("duration sent")
+        stopwatch.started = false
+        gpio.trig(inpin, "up", motion_start)
+        debug("resetting pin")
+    end
 end
+
+stopwatch = {}
+stopwatch.started = false
+stopwatch.start = tmr.time()
 
 gpio.trig(inpin, "up", motion_start)
